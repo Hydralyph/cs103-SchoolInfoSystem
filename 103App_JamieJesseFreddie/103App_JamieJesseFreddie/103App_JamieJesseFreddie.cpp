078de917f8fc4de5c525943b4407a380df113399
Jamie Adaway <270116757@yoobeestudent.ac.nz>
2021-05-31 13:45:13 +1200

    First Commit, Added Empty Project Solution.

---
+++ 103App_JamieJesseFreddie/103App_JamieJesseFreddie/103App_JamieJesseFreddie.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//-----Structs and Function Prototypes-----
+
+//-----------------------------------------
+
+int main()
+{
+    cout << "lolxd\n";
+
+    return 0;
+}
