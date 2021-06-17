5554a3c8c1ae16ba8e9c60ca486a426a9c232879
Hydralyph <acidicmc@gmail.com>
2021-06-17 14:01:49 +1200

    Updated the Project file slightly, for cleaner readability. Also started merging code into the main project.

--- 103App_JamieJesseFreddie/103App_JamieJesseFreddie/103App_JamieJesseFreddie.cpp
+++ 103App_JamieJesseFreddie/103App_JamieJesseFreddie/103App_JamieJesseFreddie.cpp
@@ -6,11 +6,28 @@ using namespace std;
 
 //-----Structs and Function Prototypes-----
 
+
+// CODE SECTION - Jamie
+
+void Parent_AccessNotices();    // These two functions are just placeholders and could change return types or arguments
+void Parent_AccessChildRecords();
+
+// CODE SECTION - End
+
+
 //-----------------------------------------
 
 int main()
 {
-    cout << "lolxd\n";
+
 
     return 0;
 }
+
+// CODE SECTION - Jamie
+
+void Parent_AccessNotices() {
+    fstream fs;
+}
+
+// CODE SECTION - End
