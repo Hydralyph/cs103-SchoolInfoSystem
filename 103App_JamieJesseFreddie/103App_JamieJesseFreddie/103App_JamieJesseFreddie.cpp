3cf9bd45867a27c513575e79c3d8750861dfb9e2
Hydralyph <acidicmc@gmail.com>
2021-06-17 14:33:46 +1200

    Updated solution manually, readding to repo.

--- 103App_JamieJesseFreddie/103App_JamieJesseFreddie/103App_JamieJesseFreddie.cpp
+++ 103App_JamieJesseFreddie/103App_JamieJesseFreddie/103App_JamieJesseFreddie.cpp
@@ -7,10 +7,18 @@ using namespace std;
 
 //-----Structs and Function Prototypes-----
 
+// CODE SECTION - Jamie
+
+void Parent_AccessNotices();    // These two functions are just placeholders and could change return types or arguments
+void Parent_AccessChildRecords();
+
+// CODE SECTION - End
+
 //-----------------------------------------
 
 int main()
 {
+
     // Jesse Simpson - Start
     int option; // Stores the user's option.
     ifstream inputDetails; // File identifier for reading purposes
@@ -75,3 +83,12 @@ int main()
 
         return 0;
  }
+
+
+// CODE SECTION - Jamie
+
+void Parent_AccessNotices() {
+    fstream fs;
+}
+
+// CODE SECTION - End
